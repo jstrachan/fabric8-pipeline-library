19541ce3e53f5fc9683f4f377a45438097dd17f0
James Strachan <james.strachan@gmail.com>
2017-10-13 16:06:02 +0100

    add a helper function for the test config

---
+++ vars/fabric8EETestFindLocal.groovy
@@ -0,0 +1,30 @@
+#!/usr/bin/groovy
+import io.fabric8.Fabric8Commands
+import io.fabric8.Utils
+
+def call(Map parameters = [:]) {
+
+  // TODO could we detect this from an env var maybe???
+
+  def flow = new Fabric8Commands()
+  def utils = new Utils()
+
+  def ns = parameters.get('namespace', "fabric8")
+
+  echo "Looking for fabric8 installation in namespace ${ns}"
+
+  def url = flow.getServiceURL("fabric8", ns)
+  if (!url) {
+    throw new Exception("Could not find service fabric8 in namespace ${ns}")
+  }
+  def platform = utils.supportsOpenShiftS2I() ? "fabric8-openshift" : "fabric8-kubernetes"
+
+  echo "found fabric8 at ${url} with platform ${platform}"
+
+  return """
+
+  export TARGET_URL="${url}"
+  export TEST_PLATFORM="${platform}"
+  
+"""
+}
