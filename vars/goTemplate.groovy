f406aad26618f2ce7504090a0d95fc9c41c5d136
James Strachan <james.strachan@gmail.com>
2017-10-13 17:55:43 +0100

    update builder clients

--- vars/goTemplate.groovy
+++ vars/goTemplate.groovy
@@ -7,7 +7,7 @@ def call(Map parameters = [:], body) {
     def label = parameters.get('label', defaultLabel)
 
     def goImage = parameters.get('goImage', 'fabric8/go-builder:1.8.1.2')
-    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:v703b6d9')
+    def clientsImage = parameters.get('clientsImage', 'fabric8/builder-clients:v616a177')
     def inheritFrom = parameters.get('inheritFrom', 'base')
     def jnlpImage = (flow.isOpenShift()) ? 'fabric8/jenkins-slave-base-centos7:0.0.1' : 'jenkinsci/jnlp-slave:2.62'
 
